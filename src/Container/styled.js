43ced2d91d42e7c2ba9004b890fb6ace8dc0b22c
Anna Sniegula <anna.sniegula@gmail.com>
2021-07-17 21:50:48 +0200

    styled-components to Container added

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,6 +1,8 @@
-.container {
+import styled from "styled-components";
+
+export const StyledContainer = styled.main`
     font-family: 'Lato', sans-serif;
     margin: 0 auto;
     max-width: 800px;
     padding: 20px;
-}
+`;
