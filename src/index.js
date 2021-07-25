94a7c7f6c094a1ba98b5cb1caa4f474c96821c92
Anna Sniegula <anna.sniegula@gmail.com>
2021-07-25 19:59:36 +0200

    GlobalStyle created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme.js";
+import { GlobalStyle } from "./GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <App />
+      <GlobalStyle />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
