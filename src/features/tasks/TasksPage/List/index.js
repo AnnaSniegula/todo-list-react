9cbc5bd73cd1e9d80267824e5c2eeb5004407597
Anna Sniegula <anna.sniegula@gmail.com>
2021-09-04 22:29:02 +0200

    Link to Content component added

---
+++ src/features/tasks/TasksPage/List/index.js
@@ -0,0 +1,42 @@
+import { TasksList, Item, Content, Button} from "./styled";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectTasks,
+  toggleTaskDone,
+  removeTask,
+  selectHideDone
+} from "../../tasksSlice";
+import { Link } from "react-router-dom";
+
+
+const List = () => {
+  const tasks = useSelector(selectTasks);
+  const hideDone = useSelector(selectHideDone);
+  const dispatch = useDispatch();
+  return (
+    <TasksList>
+      {tasks.map(task => (
+        <Item
+          key={task.id}
+          hidden={task.done && hideDone}
+        >
+          <Button toggleDone
+            onClick={() => dispatch(toggleTaskDone(task.id))}
+          >
+            {task.done ? "✓" : ""}
+          </Button>
+          <Content done={task.done}>
+            <Link to={`/zadania/${tasks.id}`}>{tasks.content}</Link>
+          </Content>
+          <Button remove
+            onClick={() => dispatch(removeTask(task.id))}
+          >
+            🗑
+          </Button>
+        </Item>
+      ))}
+    </TasksList >
+  );
+
+};
+export default List;
