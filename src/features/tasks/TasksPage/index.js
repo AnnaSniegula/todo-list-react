962b1871a949d9c0f5c3bdeac17331eb993f7e50
Anna Sniegula <anna.sniegula@gmail.com>
2021-09-05 22:54:53 +0200

    function Search and Search file created and styled

--- src/features/tasks/TasksPage/index.js
+++ src/features/tasks/TasksPage/index.js
@@ -4,6 +4,7 @@ import Buttons from "./Buttons";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
 import Container from "../../../common/Container";
+import Search from "../Search";
 
 
 function TasksPage() {
@@ -12,6 +13,9 @@ function TasksPage() {
       <Header title="Lista zadań" />
       <Section title="Dodaj nowe zadanie"
         body={<Form />}
+      />
+       <Section title="Wyszukiwarka"
+        body={<Search />}
       />
       <Section title="Lista zadań"
         body={<List />}
