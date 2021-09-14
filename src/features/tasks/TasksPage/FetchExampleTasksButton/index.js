f37dec38cfa641189d4081a6b1edef6f6ecfb205
Anna Sniegula <anna.sniegula@gmail.com>
2021-09-14 22:24:25 +0200

    fetchExapmeTasksButton created

---
+++ src/features/tasks/TasksPage/FetchExampleTasksButton/index.js
@@ -0,0 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+import { fetchExampleTasks, selectLoading } from "../../tasksSlice";
+import Button from "../Buttons";
+
+export default () => {
+    const dispatch = useDispatch();
+    const loading = useSelector(selectLoading);
+
+    return (
+        <Button disabled={loading} onClick={() => dispatch(fetchExampleTasks())}>
+            {
+                loading
+                    ? "Ładowanie..."
+                    : "Pobierz przykładowe zadania"
+            }
+
+        </Button>
+    );
+};
