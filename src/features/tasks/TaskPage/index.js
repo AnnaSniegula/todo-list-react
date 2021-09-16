5b405834c84f61bd381e7ae0e80351fb7fe029cb
Anna Sniegula <anna.sniegula@gmail.com>
2021-09-16 22:05:08 +0200

    added Styled to Container

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,6 +1,6 @@
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
-import Container from "../../../common/Container";
+import StyledContainer from "../../../common/Container";
 import { useParams } from "react-router";
 import { useSelector } from "react-redux";
 import { getTaskById } from "../tasksSlice"
@@ -9,16 +9,17 @@ export const TaskPage = () => {
     const { id } = useParams();
     const task = useSelector(state => getTaskById(state, id));
     return (
-        <Container>
+        <StyledContainer>
             <Header title="Szczegóły zadania" />
-            <Section title={task ? task.content : "Nie znaleziono zadania"}
+            <Section
+                title={task ? task.content : "Nie znaleziono zadania"}
                 body={!!task && (
                     <>
                         <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
                     </>
                 )}
             />
-        </Container>
+        </StyledContainer>
     );
 };
 
