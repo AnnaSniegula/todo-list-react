691d80e7ccd23ecb7d46c3781af9b1a4449e13a3
Anna Sniegula <anna.sniegula@gmail.com>
2021-09-18 22:12:09 +0200

    max-width changed from 800 to 900px

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const StyledContainer = styled.main`
     font-family: 'Lato', sans-serif;
     margin: 0 auto;
-    max-width: 800px;
+    max-width: 900px;
     padding: 20px;
 `;
