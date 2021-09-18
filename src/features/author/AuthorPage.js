c97783a6d6b7f3d3019318a7e9e22c570e8c43f9
Anna Sniegula <anna.sniegula@gmail.com>
2021-09-18 22:13:38 +0200

    content written and styled

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -1,12 +1,22 @@
 import StyledContainer from "../../common/Container";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
+import { Body } from "./styled";
 
 export const AuthorPage = () => (
     <StyledContainer>
         <Header title="O autorze" />
         <Section title="Anna Śnieguła"
-            body={<>nana</>}
+            body={<>
+                <Body>
+                    <p>Szukając nowych ścieżek zawodowych i rozwojowych wybrałam programowanie.
+                        Tworzenie czegoś od początku, a przy tym obserwacja wizualnego efektu moich wysiłków sprawiają mi bardzo dużą satysfakcję. 🔥</p>
+                    <p>Godziny spędzone przy komputerze równoważę aktywnością fizyczną.
+                        Miesiące ciepłe spędzam na świeżym powietrzu. Lubię biegać, jeździć rowerem, spacerować w lesie i pływać kajakiem.
+                        Resztę wolnego czasu pochłaniają mi pszczoły, które hodujemy wraz z mężem 🐝.
+                        Czas jesienno-zimowy spędzam głównie w domu czytając książki, ćwicząc jogę i odlewając woskowe świece. Delektuję się przy tym miodem z własnej pasieki i miodem pitnym własnej produkcji. Na zdrowie! 🥃</p>
+                </Body>
+            </>}
         />
     </StyledContainer>
 );
---
+++ src/features/author/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Body = styled.div`  
+    padding: 20px;
+    margin-left: 20px;
+`;
