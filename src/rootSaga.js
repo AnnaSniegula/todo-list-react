e399a452ebcb838ee60ea3fd49baf8a5ded2ba28
Anna Sniegula <anna.sniegula@gmail.com>
2021-09-20 21:06:19 +0200

    rootsaga moved to src

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,6 +1,6 @@
 import { all } from "redux-saga/effects";
 import { rootSaga } from "./features/tasks/tasksSaga";
-import { tasksSaga } from "./tasksSaga";
+import { tasksSaga } from "./features/tasks/tasksSaga";
 
 export default function* rootSaga() {
     yield all([
