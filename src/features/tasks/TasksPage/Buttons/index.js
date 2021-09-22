218d6ba08481bf957c6377c43bf9ea96f195b521
Anna Sniegula <anna.sniegula@gmail.com>
2021-09-22 20:20:05 +0200

    button disabled when there is no done task

--- src/features/tasks/TasksPage/Buttons/index.js
+++ src/features/tasks/TasksPage/Buttons/index.js
@@ -3,9 +3,9 @@ import {
     selectTasks,
     toggleHideDone,
     setAllDone,
-    fetchExampleTasks,
     selectHideDone,
     selectAreAllTasksDone,
+    selectIsAnyTaskDone
 } from "../../tasksSlice";
 import { ButtonsContainer } from "./styled";
 import Button from "../Button";
@@ -14,13 +14,16 @@ const Buttons = () => {
     const tasks = useSelector(selectTasks);
     const hideDone = useSelector(selectHideDone);
     const areAllTasksDone = useSelector(selectAreAllTasksDone);
+    const isAnyTaskDone = useSelector(selectIsAnyTaskDone);
 
     const dispatch = useDispatch();
     return (
         <ButtonsContainer>
             {tasks.length > 0 && (
                 <>
-                    <Button onClick={() => dispatch(toggleHideDone())} >
+                    <Button onClick={() => dispatch(toggleHideDone())} 
+                    disabled={!isAnyTaskDone}
+                    >
                         {hideDone ? "Pokaż" : "Ukryj"} ukończone
                     </Button>
                     <Button
